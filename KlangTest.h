4f80eaa73eaa04947462f7a527a647d923a5bb1f
parry-a <alexparry0201@gmail.com>
2024-12-09 11:17:51 +0000

    Testing

---
+++ KlangTest.h
@@ -0,0 +1,17 @@
+//
+//  KlangTest.h
+//  KlangTest
+//
+//  Created by Alex Parry on 05/12/2024.
+//
+
+#ifndef KlangTest_h
+#define KlangTest_h
+#include <klang.h>
+
+class MyEffect : public Pedal {
+    public: 
+    
+}
+
+#endif /* KlangTest_h */
